c566fcee27b9fd9ea0aaf752d0ff54c29a30ff71
wuhui.zhu <wuhui.zhu@platenogroup.com>
2016-09-05 15:28:38 +0800

    引入vue的其他常用组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App'
+// import Router from 'vue-router'
+// import Vuex from 'vuex'
+
 // 设置网页的dpr
 import LibFlexible from 'lib-flexible'
+
+// Vue.use(Vuex);
 
 /* eslint-disable no-new */
 new Vue({
